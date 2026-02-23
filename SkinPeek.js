49d0c6edd19d12a06e03cb473b3730b5dd1f434c
mistralwz <blongnh@gmail.com>
2026-02-23 15:44:35 +0700

    Optimize: Inline shardingLogic.js into SkinPeek.js

--- SkinPeek.js
+++ SkinPeek.js
@@ -1,8 +1,71 @@
 import { isMainThread } from 'worker_threads';
+import { ShardingManager } from 'discord.js';
 import { loadConfig } from "./misc/config.js";
 
 if (isMainThread) {
-    import('./shardingLogic.js').then(module => module.startShardingManager());
+    const config = loadConfig();
+
+    const manager = new ShardingManager('./SkinPeek.js', {
+        token: config.token,
+        mode: "worker",
+        totalShards: config.shards || "auto"
+    });
+
+    let allShardsReady = false;
+    const sendAllShardsReady = () => {
+        manager.broadcastEval((client) => client.skinPeekShardMessageReceived({ type: "shardsReady" }));
+    }
+
+    console.log("[Shards] Starting spawn");
+
+    manager.on("shardCreate", (shard) => {
+        console.log(`[Shard ${shard.id}] Spawned at ${new Date().toISOString()}`);
+
+        shard.on("death", () => {
+            console.error(`[Shard ${shard.id}] Died at ${new Date().toISOString()}`);
+        });
+
+        shard.on("disconnect", () => {
+            console.warn(`[Shard ${shard.id}] Discord Websocket Disconnected at ${new Date().toISOString()}`);
+        });
+
+        shard.on("reconnecting", () => {
+            console.log(`[Shard ${shard.id}] Attempting to reconnect at ${new Date().toISOString()}`);
+        });
+
+        shard.on("error", (error) => {
+            console.error(`[Shard ${shard.id}] Error at ${new Date().toISOString()}:`, error);
+        });
+
+        if (allShardsReady) {
+            // this shard was respawned, tell it that all shards are ready
+            console.log(`[Shards] Sending shardsReady to respawned shard ${shard.id} at ${new Date().toISOString()}`);
+            shard.on("ready", () => {
+                console.log(`[Shard ${shard.id}] Ready at ${new Date().toISOString()}`);
+                sendAllShardsReady();
+            });
+        }
+
+        shard.on("message", (message) => {
+            // console.log(`[Shard ${shard.id}] Message: ${JSON.stringify(message)}`);
+            if (message === "shardReady" && allShardsReady) sendAllShardsReady();
+        });
+    });
+
+    manager.on("error", (error) => {
+        console.error("[Shards] Manager error:", error);
+    });
+
+    manager.spawn({
+        timeout: config.shardReadyTimeout,
+    }).then(() => {
+        allShardsReady = true;
+        console.log(`[Shards] All shards spawned and ready at ${new Date().toISOString()}`);
+        sendAllShardsReady();
+    }).catch(err => {
+        console.error("[Shards] Failed to spawn shards:", err);
+        process.exit(1);
+    });
 } else {
     import('./botLogic.js').then(module => module.startBotWorker());
 }
